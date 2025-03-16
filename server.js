3b6b42e7b04d2b695d664c6dde8243e825f6d6f3
Saniddhya Dubey <sani@Saniddhyas-MacBook-Air-2.local>
2025-03-16 16:10:01 -0400

    Changed the data fecthing from mongoDB database & added new endpoint to add new data points in the chart

--- server.js
+++ server.js
@@ -1,34 +1,49 @@
 // Budget API
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const url = "mongodb://127.0.0.1:27017/nbad";
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
+//Connecting to the mongoose collection:
+mongoose.connect(url).then(() => {
+  console.log("connected to database!");
+});
+
+// Creating the schemal model for chart data
+const ChartData = new mongoose.Schema({
+  title: { type: String },
+  budget: { type: Number },
+  color: { type: String, min: 6 },
+});
+
+// Adding the Chart Collection in mongoose Collection:
+const Chart = mongoose.model("Chart", ChartData);
+
 app.use(cors());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
+app.get("/budget", async (req, res) => {
+  const data = await Chart.find();
+  res.json(data);
+});
 
-
-app.get('/budget', (req, res) => {
-    res.json(budget);
+app.post("/add-budget-data", async (req, res) => {
+  const { title, budget, color } = req.body;
+  const chart_data = new Chart({ title, budget, color });
+  const response = await chart_data.save();
+  if (!response) {
+    console.log("Error inserting document in the database");
+    res.json("Error inserting document in the database");
+  }
+  console.log("Document Added in the database!");
+  res.json("Document Added in the database!");
 });
 
 app.listen(port, () => {
-    console.log(`API served at http://localhost:${port}`);
+  console.log(`API served at http://localhost:${port}`);
 });
